207a1baa370d3c7429a929843e51252d7811294d
diwei77chen <diwei77chen@gmail.com>
2020-10-29 16:18:36 +1100

    created mutiple react buttons

---
+++ multiple_react_button/like_button.js
@@ -0,0 +1,26 @@
+'use strict';
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked comment number ' + this.props.commentID;
+    }
+
+    return React.createElement(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    const commentID = parseInt(domContainer.dataset.commentid, 10);
+    ReactDOM.render(React.createElement(LikeButton, { commentID: commentID }), domContainer);
+  })
